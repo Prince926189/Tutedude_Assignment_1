59604c1fb5138c1485c9083c3c5eb0b65f66bec4
Prince926189 <panditpeince88435@gmail.com>
2025-09-27 15:06:57 +0530

    First commit - completed coding task

---
+++ task_1.py
@@ -0,0 +1,12 @@
+x = int(input("Enter the first number : "))
+y = int(input("Enter the second number : "))
+
+a = x + y 
+b = x - y 
+c = x * y 
+d = x / y 
+
+print("Addition : ", a)
+print("Subtraction : ",b)
+print("MUltiplaction : ",c)
+print("Division : ",d)
